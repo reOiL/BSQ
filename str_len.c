c66da11296d82ceabc8c821d014b988d738db2c9
Jashootmee Raelene <jraelene@ga-h4.21-school.ru>
2019-07-21 13:36:24 +0300

    strlen

--- str_len.c
+++ str_len.c
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   func_prot.h                                        :+:      :+:    :+:   */
+/*   str_len.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jraelene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/21 12:29:04 by jraelene          #+#    #+#             */
-/*   Updated: 2019/07/21 13:36:04 by jraelene         ###   ########.fr       */
+/*   Created: 2019/07/21 13:31:56 by jraelene          #+#    #+#             */
+/*   Updated: 2019/07/21 13:35:14 by jraelene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FUNC_PROT_H
-# define FUNC_PROT_H
+#include "func_prot.h"
 
-void	ft_putchar(char c);
+int		ft_strlen(char *str)
+{
+	int i;
 
-void	ft_putstr(char *str);
-
-int		ft_atoi(char *str);
-
-int		ft_strlen(char *str);
-
-#endif
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
