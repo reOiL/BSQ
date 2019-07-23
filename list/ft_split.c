d0516d4ccc89b0e10aa52ba71e5ba7e9f1a011f1
Johnny Webber <jwebber@ga-h2.21-school.ru>
2019-07-23 20:42:03 +0300

    file reformated

--- list/ft_split.c
+++ list/ft_split.c
@@ -1,28 +1,36 @@
-//
-// Created by reoil on 22.07.19.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/23 20:35:17 by jwebber           #+#    #+#             */
+/*   Updated: 2019/07/23 20:41:47 by jwebber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../ft/func_prot.h"
 #include "ft_list.h"
 
-t_list  *ft_split(char *str)
+t_list	*ft_split(char *str)
 {
-    t_list  *ret;
-    int     i;
-    int     j;
+	t_list	*ret;
+	int		i;
+	int		j;
 
-    ret = 0;
-    i = 0;
-    j = 0;
-    while (str[i])
-    {
-        if(str[i] == '\n')
-        {
-            str[i] = '\0';
-            ft_list_push_back(&ret, ft_strdup(&str[j]));
-            j = i + 1;
-        }
-        i++;
-    }
-    return (ret);
+	ret = 0;
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+		{
+			str[i] = '\0';
+			ft_list_push_back(&ret, ft_strdup(&str[j]));
+			j = i + 1;
+		}
+		i++;
+	}
+	return (ret);
 }
