9d00e5d4f2cc4eef76ff64da946ea2d2afd91b3e
Johnny Webber <jwebber@ga-h2.21-school.ru>
2019-07-23 11:26:24 +0300

    Start make protect

--- io_support.c
+++ io_support.c
@@ -6,7 +6,7 @@
 /*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/21 12:41:44 by jwebber           #+#    #+#             */
-/*   Updated: 2019/07/23 09:01:05 by jwebber          ###   ########.fr       */
+/*   Updated: 2019/07/23 11:25:53 by jwebber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int		count_new_line(char	*str)
+{
+	int	i;
+	int	n;
+
+	n = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			n++;
+		i++;
+	}
+	return (n);
+}
+
 char	*io_read(int fd)
 {
 	char	*result;
 	char	buffer[BUFF_SIZE + 1];
 	int		res;
 	int		taked;
+	int		new_line_count;
 
+	new_line_count = 0;
 	taked = 0;
 	result = (char *)malloc(sizeof(char) * 1);
 	while ((res = read(fd, buffer, BUFF_SIZE)))
@@ -31,6 +49,7 @@ char	*io_read(int fd)
 			break ;
 		buffer[res] = '\0';
 		result = ft_realloc(result, res);
+		new_line_count += count_new_line(buffer);
 		ft_strcpy(&result[taked], buffer);
 		taked += res;
 	}
